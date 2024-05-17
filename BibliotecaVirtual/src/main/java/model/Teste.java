8a51fd64c28864259b00393a293c8c1a73466de1
Isa <isaatanisue@gmail.com>
2024-05-17 09:49:57 -0300

    Criação TelaLogin

--- BibliotecaVirtual/src/main/java/model/Teste.java
+++ BibliotecaVirtual/src/main/java/model/Teste.java
@@ -2,6 +2,6 @@ package model;
 
 public class Teste {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        TelaLogin login = new TelaLogin();
     }
 }
