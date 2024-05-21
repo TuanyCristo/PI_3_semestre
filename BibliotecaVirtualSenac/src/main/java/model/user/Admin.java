809eb6083be3528148cc4e8e31655e9bfff1d940
Tuany Cristo <93205336+TuanyCristo@users.noreply.github.com>
2024-05-20 22:12:07 -0300

    Atualização da classe Usuário
    Adição da classe pessoa para extender usuário comum e o administrador

--- BibliotecaVirtualSenac/src/main/java/model/user/Admin.java
+++ BibliotecaVirtualSenac/src/main/java/model/user/Admin.java
@@ -1,16 +1,14 @@
 package model.user;
 //Aqui nós utilizamos herança
-public class Admin extends Usuario{
+public class Admin extends Pessoa{
     private String cargo;
-    private String cpf;
 
     public Admin() {
     }
 
-    public Admin(int idUsuario, String nome, String email, String senha, String cargo, String cpf) {
-        super(idUsuario, nome, email, senha);
+    public Admin(int idUsuario, String nome, String emailInstitucional, String senha, String cargo) {
+        super(idUsuario, nome, emailInstitucional, senha);
         this.cargo = cargo;
-        this.cpf = cpf;
     }
 
     public String getCargo() {
@@ -19,14 +17,6 @@ public class Admin extends Usuario{
 
     public void setCargo(String cargo) {
         this.cargo = cargo;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
     }
 
     
--- BibliotecaVirtualSenac/src/main/java/model/user/Usuario.java
+++ BibliotecaVirtualSenac/src/main/java/model/user/Usuario.java
@@ -3,72 +3,26 @@ package model.user;
 import java.util.ArrayList;
 import model.livros.Livro;
 
-public class Usuario {
-private int idUsuario;
-private String nome;
-private String emailInstitucional;
-private String senha;
-private ArrayList<Livro> listaLivros;
+public class Usuario extends Pessoa {
 
-public Usuario() {
-}
+    private ArrayList<Livro> listaLivros;
 
-public Usuario(int idUsuario, String nome, String emailInstitucional, String senha) {
-    this.idUsuario = idUsuario;
-    this.nome = nome;
-    this.emailInstitucional = emailInstitucional;
-    this.senha = senha;
-    this.listaLivros = new ArrayList<Livro>();
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario, String nome, String emailInstitucional, String senha) {
+        super(idUsuario, nome, emailInstitucional, senha);
+        this.listaLivros = new ArrayList<Livro>();
+    }
+
+    public ArrayList<Livro> getListaLivros() {
+        return listaLivros;
+    }
+
+    public void setListaLivros(ArrayList<Livro> listaLivros) {
+        this.listaLivros = listaLivros;
+    }
+
     
-}
-
-public int getIdUsuario() {
-    return idUsuario;
-}
-
-public void setIdUsuario(int idUsuario) {
-    this.idUsuario = idUsuario;
-}
-
-public String getNome() {
-    return nome;
-}
-
-public void setNome(String nome) {
-    this.nome = nome;
-}
-
-public String getEmailInstitucional() {
-    return emailInstitucional;
-}
-
-public void setEmailInstitucional(String emailInstitucional) {
-    this.emailInstitucional = emailInstitucional;
-}
-
-public String getSenha() {
-    return senha;
-}
-
-public void setSenha(String senha) {
-    this.senha = senha;
-}
-
-public ArrayList<Livro> getListaLivros() {
-    return listaLivros;
-}
-
-public void setListaLivros(ArrayList<Livro> listaLivros) {
-    this.listaLivros = listaLivros;
-}
-
-@Override
-public String toString() {
-    return "Usuario [idUsuario=" + idUsuario + ", nome=" + nome + ", emailInstitucional=" + emailInstitucional
-            + ", senha=" + senha + "]";
-}
-
-
-
-
+   
 }
