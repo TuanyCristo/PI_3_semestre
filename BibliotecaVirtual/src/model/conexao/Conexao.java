9ca0a3da5a272f7c5644be73599bec4ba74ae0f9
Tuany Cristo <t.cristo@hotmail.com>
2024-05-22 11:13:33 -0300

    Classe conexao

--- BibliotecaVirtual/src/model/conexao/Conexao.java
+++ BibliotecaVirtual/src/model/conexao/Conexao.java
@@ -1,16 +1,56 @@
 package model.conexao;
 
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String url = "jdbc:mysql://localhost:3306/biblioteca/";
     private static final String user = "root";
     private static final String password = "";
-=======
-public class ConexaoBanco {
->>>>>>> parent of 2e58967 (conexao DB)
     
+    private Connection conn = null;
+    
+   
+
+public class ConexaoBanco {
+
+        public ConexaoBanco() {         
+            
+        }
+        
+        public Connection criaConexao(){
+            try {
+                if(conn == null){
+                    Class.forName("org.gjt.mm.mysql.Driver");
+                    conn = DriverManager.getConnection(url, user, password);
+                } else if (conn.isClosed()) {
+                    conn = null;
+                    return criaConexao();
+                }
+            } catch (ClassNotFoundException e) {
+                //TODO: use um sistema de log apropriado.
+                e.printStackTrace();
+            } catch (SQLException e) {
+                //TODO: use um sistema de log apropriado.
+                e.printStackTrace();
+            }
+            return conn;
+        }
+        
+        public void fechaConexao() {
+            if (conn != null) {
+              try {
+                conn.close();
+              } catch (SQLException e) {
+                //TODO: use um sistema de log apropriado.
+                e.printStackTrace();
+              }
+            }
+}
+    
+        
+    }
 }
