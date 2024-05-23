a9c70b2228746b8cac5400bd3e4fbc7e56c0e406
Tuany Cristo <t.cristo@hotmail.com>
2024-05-23 01:39:58 -0300

    DAO Reserva e Estoque
    Não tem códigos ainda, criei para adiantar

---
+++ BibliotecaVirtual/src/model/dao/ReservaDAO.java
@@ -0,0 +1,35 @@
+
+package model.dao;
+
+import java.util.List;
+import java.util.Optional;
+import model.reserva.Reserva;
+
+public class ReservaDAO implements InterfaceDAO<Reserva, Integer>{
+
+    @Override
+    public boolean inserirItem(Reserva objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean alterarItem(int id, Reserva objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Reserva> listarItens() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean deletarItem(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Optional<Reserva> buscarId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
