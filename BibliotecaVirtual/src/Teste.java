9a8b82f62e34a8dc92d1f6ba3db06b271bd054ac
Isa <isaatanisue@gmail.com>
2024-05-28 09:24:44 -0300

    Revert "funcionando"
    This reverts commit 3af1bda1cba92c4898ac3b8fa6f97f048ad034c8.

--- BibliotecaVirtual/src/Teste.java
+++ BibliotecaVirtual/src/Teste.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import model.dao.LivroDAO;
+import model.livros.Livro;
 
 public class Teste {
     public static void main(String[] args) {
             
-        //main só para teste
+        
         
     }
 }
